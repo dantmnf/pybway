135c5a7d1847f442a06cb9d35f64c7ef5d71618d
dantmnf <dantmnf2@gmail.com>
2012-12-23 15:10:25 +0800

    fixed slot functions

--- pybway.py
+++ pybway.py
@@ -1,31 +1,39 @@
 #!/usr/bin/env python3
 # file pybway.py
 import sys
-from PyQt4 import QtGui, QtCore, QtWebKit
-class Metro(QtWebKit.QWebView):
+import os
+from PyQt4.QtGui    import *
+from PyQt4.QtCore   import *
+from PyQt4.QtWebKit import *
+class Metro(QWebView):
   
   def __init__(self, parent=None):
-    QtWebKit.QWebView.__init__(self, parent)
-    self.setWindowFlags(QtCore.Qt.WindowStaysOnBottomHint | QtCore.Qt.FramelessWindowHint)
-    if len(QtGui.QApplication.arguments()) <= 1:
-      self.load(QtCore.QUrl("https://metro-subway.rhcloud.com/MT.php"))
+    QWebView.__init__(self, parent)
+    self.setWindowFlags(Qt.WindowStaysOnBottomHint | Qt.FramelessWindowHint)
+    if len(QApplication.arguments()) <= 1:
+      self.load(QUrl("https://metro-subway.rhcloud.com/MT.php"))
     else:
-      self.load(QtCore.QUrl(QtGui.QApplication.arguments()[1]))
+      self.load(QUrl(QApplication.arguments()[1]))
     self.showFullScreen()
-    self.connect(self.page().mainFrame(), QtCore.SIGNAL("javaScriptWindowObjectCleared()"), self.javaScriptWindowObjectCleared)
-    defaultSettings = QtWebKit.QWebSettings.globalSettings()
-    defaultSettings.setAttribute(QtWebKit.QWebSettings.JavascriptEnabled, True)
-    defaultSettings.setAttribute(QtWebKit.QWebSettings.PluginsEnabled, True)
-    defaultSettings.setAttribute(QtWebKit.QWebSettings.LocalContentCanAccessRemoteUrls, True)
-    defaultSettings.setAttribute(QtWebKit.QWebSettings.LocalStorageDatabaseEnabled, True)
+    QObject.connect(self.page().mainFrame(), SIGNAL("javaScriptWindowObjectCleared()"), self.javaScriptWindowObjectCleared)
+    defaultSettings = QWebSettings.globalSettings()
+    defaultSettings.setAttribute(QWebSettings.JavascriptEnabled, True)
+    defaultSettings.setAttribute(QWebSettings.PluginsEnabled, True)
+    defaultSettings.setAttribute(QWebSettings.LocalContentCanAccessRemoteUrls, True)
+    defaultSettings.setAttribute(QWebSettings.LocalStorageDatabaseEnabled, True)
     
   def javaScriptWindowObjectCleared(self):
-    self.page().mainFrame().addToJavaScriptWindowObject("MetroView", self)
+    hr=self.page().mainFrame().addToJavaScriptWindowObject("MetroView", self)
+    print(hr)
     
+  @pyqtSlot(str)
   def QtAlert(self, _str):
-    QtGui.QMessageBox.information(self, "QtAlert", _str)
-    
-  def System(self, cmd): os.system(cmd)  #use os.system instead
+    QMessageBox.information(self, "QtAlert", _str)
+  
+  @pyqtSlot(str)
+  def System(self, cmd):
+    print('system',cmd)
+    os.system(cmd)  #use os.system instead
   
   def RunLua(self, _str): pass        #everything about Lua won't be implemented
   def RunLuaString(self, _str): pass
@@ -35,7 +43,7 @@ class Metro(QtWebKit.QWebView):
   def Hide(self): hide()   #what ru doing?
   
 if __name__ == '__main__':
-  app = QtGui.QApplication(sys.argv)
+  app = QApplication(sys.argv)
   w = Metro()
   w.show()
   sys.exit(app.exec_())
